986edf0b0c4f0a27523c2e1567b4e7643c0d9219
Nguyennhuy <khanhandlinh12345@gmail.com>
2020-12-11 13:13:59 +0700

    rm7

--- index.php
+++ index.php
@@ -1,145 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Hệ thống bệnh viện</title>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="./assets/fonts/fontawesome-free-5.14.0-web/css/all.min.css ">
-      <link rel="stylesheet" href="./assets/css/form.css">
-      <link rel="stylesheet" href="./assets/css/main.css">
-      <link rel="stylesheet" href="./assets/css/base.css">
-   </head>
-   <body style="overflow: hidden; margin: 0;">
-      <div class="app">
-      <!-- <div class="div1" style="color: white;">
-         <span><img class="div1-img" src="./assets/img/logo.png" alt=""></span>
-         <span> Phân </span>
-         <span> Tích </span>
-         <span> Thiết </span>
-         <span> Kế </span>
-         <span> Hệ </span>
-         <span> Thống </span>
-         <span> Quản </span>
-         <span> Lý </span>
-         <span> Quầy </span>
-         <span> Thuốc </span>
-         <span> Bệnh </span>
-         <span> Viện </span>
-      </div> -->
-      <!-- <div class="nhom8">NGUYỄN NHƯ Ý</div> -->
-      <div class="grid">
-         <div class="picture">
-            <div class="picture__selection">
-               <div class="picture__selection-btn">
-                  <div class="selection-btn">
-                     <div class="btn btn-patient" style="cursor: pointer;" onclick="onLogin()">
-                        <a>Đăng Nhập</a>
-                     </div>
-                     <div class="btn btn-doctor" style="cursor: pointer;" onclick="onSingin()">
-                        <a>Đăng Ký</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <div class="modal1" id="modal__view">
-         <div class="modal__overlay"></div>
-         <div class="modal__body">
-         </div>
-         <!-- Register form -->
-         <div class="app app_form" id="login__hidden">
-            <div class="auth-form">
-               <form class="auth-form__container" method="post" action="php/user.php">
-                  <div class="auth-form__header">
-                     <h3 class="auth-form__heading">Đăng Ký</h3>
-                     <span class="auth-form__switch-btn" onclick="Login_form()">Đăng nhập</span>
-                  </div>
-                  <div class="auth-form_form">
-                     <div class="auth-form_group">
-                        <input type="text" class="auth-form__input" name="email" placeholder="Email của bạn">
-                     </div>
-                     <div class="auth-form_group">
-                        <input type="password" class="auth-form__input" name="password1" placeholder="Mật khẩu của bạn">
-                     </div>
-                     <div class="auth-form_group">
-                        <input type="password" class="auth-form__input" name="password2" placeholder="Nhập lại mật khẩu của bạn">
-                     </div>
-                  </div>
-                  <div class="auth-form__aside">
-                     <p class="auth-form__policy-text">
-                        Bằng việc đăng kí, bạn đã đồng ý với Admin Nhu Y về
-                        <a href="" class="auth-form__text-link">Điều khoản dịch vụ</a> &
-                        <a href="" class="auth-form__text-link">Chính sách bảo mật</a>
-                     </p>
-                  </div>
-                  <div class="auth-form__controls">
-                     <div class="btn1 btn--normal auth-form__controls-back" onclick="loginBack1()">TRỞ LẠI</div>
-                     <button class="btn1 btn--primary" name="DK">ĐĂNG KÝ</button>
-                  </div>
-               </form>
-               <div class="auth-form__socials">
-                  <a href="" class="auth-form__socials--facebook btn1 btn--size-s btn--with-icon">
-                  <i class="auth-form__socials-icon fab fa-facebook-square"></i>
-                  <span class="auth-form__socials-title">
-                  Kết nối với Facebook
-                  </span>
-                  </a>
-                  <a href="" class="auth-form__socials--google btn1 btn--size-s btn--with-icon">
-                  <i class="auth-form__socials-icon fab fa-google"></i>
-                  <span class="auth-form__socials-title">
-                  Kết nối với Google
-                  </span>
-                  </a>
-               </div>
-            </div>
-         </div>
-         <!-- Login form -->
-         <div class="app app_form app_form-hidden" id="loginback">
-            <div class="auth-form">
-               <form class="auth-form__container" method="post" action="php/login.php" >
-                  <div class="auth-form__header">
-                     <h3 class="auth-form__heading">Đăng nhập</h3>
-                     <span class="auth-form__switch-btn" onclick="Sign_in()">Đăng Ký</span>
-                  </div>
-                  <div class="auth-form_form">
-                     <div class="auth-form_group">
-                        <input type="text" name="email" class="auth-form__input" placeholder="Email của bạn">
-                     </div>
-                     <div class="auth-form_group">
-                        <input type="password" name="password1" class="auth-form__input" placeholder="Mật khẩu của bạn">
-                     </div>
-                  </div>
-                  <div class="auth-form__aside">
-                     <div class="auth-form__help">
-                        <a href="" class="auth-form__help-link auth-form__help-forgot">Quên mật khẩu</a>
-                        <span class="auth-form__help-separate"></span>
-                        <a href="" class="auth-form__help-link">Cần trợ giúp?</a>
-                     </div>
-                  </div>
-                  <div class="auth-form__controls">
-                     <div  class="btn1 btn--normal auth-form__controls-back" onclick="loginBack()">TRỞ LẠI</div>
-                     <button class="btn1 btn--primary" name="DN">ĐĂNG NHẬP</button>
-                  </div>
-               </form>
-               <div class="auth-form__socials">
-                  <a href="" class="auth-form__socials--facebook btn1 btn--size-s btn--with-icon">
-                  <i class="auth-form__socials-icon fab fa-facebook-square"></i>
-                  <span class="auth-form__socials-title">
-                  Kết nối với Facebook
-                  </span>
-                  </a>
-                  <a href="" class="auth-form__socials--google btn1 btn--size-s btn--with-icon">
-                  <i class="auth-form__socials-icon fab fa-google"></i>
-                  <span class="auth-form__socials-title">
-                  Kết nối với Google
-                  </span>
-                  </a>
-               </div>
-            </div>
-         </div>
-      </div>
-      <script src="./assets/js/base.js"></script>
-   </body>
-</html>
+<?php 
+
+$username = "doanbenh_root"; // Khai báo username
+        $password = "khanhandli";      // Khai báo password
+        $server   = "localhost";   // Khai báo server
+        $dbname   = "doanbenh_BenhVien1";      // Khai báo database
+        // Kết nối database
+        $connect = new mysqli($server, $username, $password, $dbname);
+        //Nếu kết nối bị lỗi thì xuất báo lỗi và thoát.
+        if ($connect->connect_error) {
+        die("Không kết nối :" . $conn->connect_error);
+        exit();} else {
+         echo "string";
+        }
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Document</title>
+    </head>
+    <body>
+       dsadsadsadsadssa
+    </body>
+    </html>
